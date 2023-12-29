1162e4c034f6e8d249a2078d3252a7589f4a4f9a
amanDev1437 <amanpratap1437@gmail.com>
2023-12-29 18:26:44 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,25 @@
+
+/*const heading = React.createElement("h1",{id:"heading"},"Hello world from React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);*/
+
+const parent = React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement(
+        "div",
+        {id:"child"},
+        React.createElement(
+            "h1",
+            {},
+            "i am h1 tag"
+        )
+    )    
+    
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);  
