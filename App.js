d9878ebe2ca1e7199aa371ff47be76dffd6b2ae4
amanDev1437 <amanpratap1437@gmail.com>
2024-01-05 12:19:18 +0530

    ep 04

--- App.js
+++ App.js
@@ -1,24 +1,71 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-//React Element
-const heading = (<h1 id="heading" className="head">Namste React using JSX</h1>);
+const Header = () =>{
+    return(
+        <div className="header">
+            <div className="logo">
+                <img className="logo" src="https://static.vecteezy.com/system/resources/thumbnails/011/468/885/small/food-logo-spoon-fork-icon-illustration-symbol-for-fast-delivery-app-restaurant-template-free-vector.jpg"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
 
-//React Functional Component
-const Title= () =>{
-    return (<h1>Namaste Functional Component</h1>);
+        </div>
+    );
 }
 
-//Composition Component
-const HeadingComponent = () =>{
-    return(<>
-        {Title()}
-        <Title />
-        <Title></Title>
-        <h2>Namste Composition Component</h2>
-    </>);
+
+
+const RestaurantCard = (props) =>{
+    return(
+        <div className="res-card">
+             <img className="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/89fccaa76f2f760e2742b9e53d32bb69"/>
+            <h4>{props.resName}</h4>
+            <h4>{props.cuisine}</h4>
+            <h4>{props.rating}</h4>
+            <h4>{props.time}</h4>
+        </div>
+    );
+}
+
+const Body = () =>{
+    return(
+        <div className="body">
+           <div className="search">Search</div>
+           <div className="res-container"> 
+             <RestaurantCard 
+             resName="Shiv foods"
+             cuisine="Biriyani, North-Indian"
+             rating="3.5 star"
+             time="38"
+
+             />
+             <RestaurantCard 
+             resName="Pal Dhaba" 
+             cuisine="Paneer, South-Indian"
+             rating="4 star"
+             time="20"
+
+             />
+           </div> 
+        </div>
+    );
+}
+const AppLayout = () =>{
+    return(
+        <div className="app">
+            <Header></Header>
+            <Body></Body>
+        </div>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent></HeadingComponent>);  
+root.render(<AppLayout></AppLayout>);  
